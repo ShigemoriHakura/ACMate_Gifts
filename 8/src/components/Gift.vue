5977fce89b5809eeb1e9560e31773ce256995aca
ShigemoriHakura <62388797+ShigemoriHakura@users.noreply.github.com>
2020-10-26 18:05:57 +0800

    更新

--- 8/src/components/Gift.vue
+++ 8/src/components/Gift.vue
@@ -1,24 +1,24 @@
 <template>
   <v-container>
     <Snowf v-if='fxType == "snow"'
-      :amount="50"
-      :size="5"
-      :speed="1.5"
-      :wind="0"
-      :opacity="0.8"
-      :swing="1"
+      :amount="amount"
+      :size="size"
+      :speed="speed"
+      :wind="wind"
+      :opacity="opacity"
+      :swing="swing"
       :image="null"
       :zIndex="null"
       :resize="true"
       color="#fff"
     />
     <Snowf v-if='fxType == "sakura"'
-      :amount="50"
-      :size="5"
-      :speed="1.5"
-      :wind="0"
-      :opacity="0.8"
-      :swing="1"
+      :amount="amount"
+      :size="size"
+      :speed="speed"
+      :wind="wind"
+      :opacity="opacity"
+      :swing="swing"
       :image="require('../../public/static/img/sakura.png')"
       :zIndex="null"
       :resize="true"
@@ -39,6 +39,12 @@ export default {
   data() {
     return {
       fxType:"",
+      speed: 1.5,
+      swing: 1,
+      wind: 0,
+      size: 5,
+      opacity: 0.8,
+      amount: 50,
     }
   },
   created() {
@@ -52,6 +58,12 @@ export default {
       var data = (await axios.get(url)).data
       if(data.result == 1){
         this.fxType = data.data.fx
+        this.speed = parseFloat(data.data.speed)
+        this.swing = parseFloat(data.data.swing)
+        this.wind = parseFloat(data.data.wind)
+        this.size = parseFloat(data.data.size)
+        this.opacity = parseFloat(data.data.opacity)
+        this.amount = parseFloat(data.data.amount)
       }
     },
   }
